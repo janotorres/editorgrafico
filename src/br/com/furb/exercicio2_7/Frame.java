d6572b3c7aec6a5004d64a6698bb1e763ff6c001
jgtorres <jgtorres@MC-3302.furb.br>
2015-04-10 19:37:57 -0300

    ty

---
+++ src/br/com/furb/exercicio2_7/Frame.java
@@ -0,0 +1,43 @@
+package br.com.furb.exercicio2_7;
+
+import java.awt.BorderLayout;
+
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+
+public class Frame extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	private int janelaLargura  = 400, janelaAltura = 400;
+	
+	public Frame() {		
+		
+		super("CG-N2_7");   
+		setBounds(300,250,janelaLargura,janelaAltura+22); 
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		getContentPane().setLayout(new BorderLayout());
+
+		GLCapabilities glCaps = new GLCapabilities();
+		Desenho desenho = new Desenho();
+		glCaps.setRedBits(8);
+		glCaps.setBlueBits(8);
+		glCaps.setGreenBits(8);
+		glCaps.setAlphaBits(8); 
+
+		GLCanvas canvas = new GLCanvas(glCaps);
+		add(canvas,BorderLayout.CENTER);
+		canvas.addGLEventListener(desenho);        
+		canvas.addKeyListener(desenho);
+		canvas.addMouseListener(desenho);
+		canvas.addMouseMotionListener(desenho);
+		canvas.requestFocus();			
+	}		
+	
+	public static void main(String[] args) {
+		new Frame().setVisible(true);
+	}
+
+}
---
+++ src/br/com/furb/exercicio2_7/Render.java
@@ -0,0 +1,34 @@
+package br.com.furb.exercicio2_7;
+
+import javax.media.opengl.DebugGL;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.glu.GLU;
+
+public class Render {
+	
+	private GL gl;
+	private GLU glu;
+	private GLAutoDrawable glDrawable;
+	
+	public void inicializate(GLAutoDrawable drawable){
+		glDrawable = drawable;
+		gl = drawable.getGL();
+		glu = new GLU();
+		glDrawable.setGL(new DebugGL(gl));
+		gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	}
+
+	public GL getGl() {
+		return gl;
+	}
+
+	public GLU getGlu() {
+		return glu;
+	}
+
+	public GLAutoDrawable getGlDrawable() {
+		return glDrawable;
+	}
+
+}
